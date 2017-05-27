116acd2edabd715754286a85c99546c7002e8ac9
RaptorDream <rdatchane.19@gmail.com>
2017-05-27 10:06:25 +0200

    Add files via upload

---
+++ discord_bot.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/ruby
+#DiscordBotProject
+#Coded in Ruby using DiscordRB Dependency
+require 'discordrb'
+bot = Discordrb::Bot.new token: 'MzE3NzUzNDQyOTI5OTk5ODc0.DAogsQ.YhxmkECIpBw2zhGoi5xpba3sgm0', client_id: 317753442929999874
+bot.message(with_text: '!ping') do |event|
+  event.respond 'Pong!'
+end
+bot.message(with_text: '!whois raptordream') do |event|
+  event.respond 'He is Rohit'
+end
+bot.message(with_text: '!whois bardane') do |event|
+event.respond 'He is Théo'
+end
+bot.message(with_text: '!whois sheepv2') do |event|
+event.respond 'He is Philippe'
+end
+bot.message(with_text: '!whois minbadton') do |event|
+event.respond 'He is Guillaume'
+end
+bot.message(with_text: '!whois feedmeforchicken') do |event|
+event.respond 'He is Yannis'
+end
+bot.message(with_text: '!whois falcondu94') do |event|
+event.respond 'He is Hugo'
+end
+bot.message(with_text: '!ITTO') do |event|
+event.respond 'Yes!'
+end
+bot.message(with_text: '!can you play shape of you?') do |event|
+event.respond 'No, I don\'t practice EarReape :no_entry: '
+end
+bot.message(with_text: '!funvideos') do |event|
+event.respond ':ok: Enjoy: https://www.youtube.com/watch?v=hlr0psXBS4U '
+end
+bot.message(with_text: '!credits') do |event|
+event.respond 'License: discordrb dependency using Discord API Developped by R4pt0r'
+end
+bot.message(with_text: '!help') do |event|
+event.respond 'Under Construction'
+end
+bot.run
